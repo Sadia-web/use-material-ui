0eeb736b7511241c0acf14948935556420e64dea
Sadia-web <sadiamim354@gmail.com>
2020-08-30 20:51:21 +0600

    code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {Button} from '@material-ui/core/Button';
 import './App.css';
+import News from './components/News/News';
+import axios from 'axios';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=ecf2254474e34f1d873adff51f51f6dc')
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  }, [])
+
+  useEffect(() => {
+    axios('https://newsapi.org/v2/top-headlines?country=us&apiKey=ecf2254474e34f1d873adff51f51f6dc')
+    .then(data => setArticles(data.data.articles))
+    
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
+    
     </div>
   );
 }
